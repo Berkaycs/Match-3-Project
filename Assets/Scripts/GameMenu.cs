4d8a7cb77c3571131b0728d3d4bd63ea501810dd
berkaypicakci <berkaypicakci@outlook.com>
2024-04-20 06:17:29 +0300

    UI006-Arayüz-Veritabanı-Bağlantısı

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class GameMenu : MonoBehaviour
 {
-    public UserData UserData;
-    public string LevelName;
-    public void UnlockedLevel()
+    private RegistrationLoginManager login;
+    private int lockedLevel = 2;
+    private int nextLevel;
+    private string LevelName;
+    public int CurrentLevel;
+
+    private void Awake()
     {
-        UserData.Level++;
-        LevelName = "Level " + UserData.Level;
-        SceneManager.LoadScene(LevelName);
+        login = GameObject.Find("LoginManager").GetComponent<RegistrationLoginManager>();
     }
 
     public void MainMenu()
@@ -19,8 +22,63 @@ public class GameMenu : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void UnlockedLevel()
+    {
+        StartCoroutine(UpdateLevel());
+    }
+
     public void Retry()
     {
-        SceneManager.LoadScene("Level " + UserData.Level);
+        LevelName = "Level " + CurrentLevel;
+        Debug.Log(LevelName);
+        SceneManager.LoadScene(LevelName);
+    }
+
+    IEnumerator UpdateLevel()
+    {
+        // Escape the username to ensure it's URL-safe
+        string username = UnityWebRequest.EscapeURL(login.loggedInUsername);
+
+        // Construct the URL with the username as a parameter
+        string url = "http://127.0.0.1/match3/updatelevel.php?user_name=" + username;
+
+        // Send the GET request to the PHP script
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                // Get the response text containing the new level data
+                string json = request.downloadHandler.text;
+                Debug.Log(json);
+
+                // Parse the JSON response to extract the new level
+                UserDataList userData = JsonUtility.FromJson<UserDataList>("{\"entries\":" + json + "}");
+
+                // Access the new level from the response
+                lockedLevel = userData.entries[0].Level + 1;
+
+                nextLevel = CurrentLevel + 1;
+
+                if (nextLevel <= lockedLevel)
+                {
+                    LevelName = "Level " + nextLevel;                   
+                }
+                else
+                {
+                    LevelName = "Level " + lockedLevel;                   
+                }
+
+                SceneManager.LoadScene(LevelName);
+
+                // Use the new level as needed in your game logic
+                Debug.Log("Locked level: " + lockedLevel);
+            }
+            else
+            {
+                Debug.LogError("Failed to update level: " + request.error);
+            }
+        }
     }
 }
