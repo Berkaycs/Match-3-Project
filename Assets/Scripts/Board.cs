5e280a7499ea4b6d31a7d8637e191bf4c408b465
Cansu <cansucakiir53@gmail.com>
2024-04-26 02:57:54 +0300

    sixth commit
    -Checking matches
    -Destroying matching tiles
    -Preventing cross and non-neighbor movements

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
+using System;
 
 public class Board : MonoBehaviour
 {
     public static Board Instance { get; private set; }
 
+
     public Row[] Rows;
+
+    //iki boyutlu dizi. sütun ve satýrý ifade eder.
     public Tile[,] Tiles { get; private set; }
 
+    //Width sütun sayýsý, Height satýr sayýsý.
     public int Width
     {
        get { return Tiles.GetLength(dimension: 0); }
@@ -31,24 +36,30 @@ public class Board : MonoBehaviour
 
     private void Start()
     {
+        //linQ ile pratik yoldan yazým.
         Tiles = new Tile[Rows.Max(selector: r=> r.Tiles1.Length),Rows.Length];
 
-        for (var y = 0; y < Height; y++)
+        for (int a = 0; a < Height; a++)
         {
 
-            for (var x = 0; x < Width; x++)
+            for (int b = 0; b < Width; b++)
             {
-                var tile = Rows[y].Tiles1[x];
-                tile.x = x;
-                tile.y = y;
-                tile.Item = ItemDatabase.Items[Random.Range(0,ItemDatabase.Items.Length-1)];
-                Tiles[y, x] = tile;
+                Tile tile = Rows[a].Tiles1[b];
+                tile.x = b;
+                tile.y = a;
+                tile.Item = ItemDatabase.Items[UnityEngine.Random.Range(0,ItemDatabase.Items.Length-1)];
+                Tiles[b, a] = tile;
             }
         }
+        Popx();
+        Popy();
     }
 
+   
     public async void Select(Tile tile)
     {
+        var control = 0;
+        
         if (!_selection.Contains(tile))
         {
             _selection.Add(tile);
@@ -59,7 +70,32 @@ public class Board : MonoBehaviour
         }
         Debug.Log($"Selected tiles at ({_selection[0].x}),({_selection[0].y}) and ({ _selection[1].x}),({ _selection[1].y})");
         await Swap(_selection[0], _selection[1]);
-        _selection.Clear();
+        if ((_selection[0].x == _selection[1].x-1 && _selection[0].y == _selection[1].y) || (_selection[0].x == _selection[1].x + 1 && _selection[0].y == _selection[1].y) || (_selection[0].y == _selection[1].y + 1 && _selection[0].x == _selection[1].x) || (_selection[0].y == _selection[1].y - 1 && _selection[0].x == _selection[1].x))
+        {
+            if (CanPopx())
+            {
+                Popx();
+                control = 1;
+            }
+            if (CanPopy())
+            {
+                Popy();
+                control = 1;
+            }
+            if (control == 0)
+            {
+                await Swap(_selection[0], _selection[1]);
+            }
+
+            _selection.Clear();
+            control = 0;           
+        }
+        else
+        {
+            await Swap(_selection[0], _selection[1]);
+            _selection.Clear();
+        }
+        
     }
 
 
@@ -82,10 +118,112 @@ public class Board : MonoBehaviour
         tile1.Icon = icon2;
         tile2.Icon = icon1;
 
+        //önceki bilgiyi tutar.
         var tile1Item = tile1.Item;
 
         tile1.Item = tile2.Item;
         tile2.Item = tile1Item;
 
+    }
+
+    private bool CanPopx()
+    {
+        for (var y=0; y < Height; y++)        
+            for (var x=0; x < Width; x++)            
+                if (Tiles[x, y].GetConnectedTilesx().Skip(1).Count() >= 2)
+                {
+                    return true;
+                }
+                
+        return false;
+    }
+    private bool CanPopy()
+    {
+        for (var y = 0; y < Height; y++)
+            for (var x = 0; x < Width; x++)
+                if (Tiles[x, y].GetConnectedTilesy().Skip(1).Count() >= 2)
+                {
+                    return true;
+                }
+
+        return false;
+    }
+    private async void Popx()
+    {
+        for (var y=0; y < Height; y++)
+        {
+            for (var x=0; x < Width; x++)
+            {
+                var tile = Tiles[x, y];
+                var connectedTiles = tile.GetConnectedTilesx();
+                if (connectedTiles.Skip(1).Count() < 2)
+                { 
+                    continue;
+                }
+                var deflateSequence = DOTween.Sequence();
+
+                foreach (var connectedTile in connectedTiles)
+                { 
+                    deflateSequence.Join(connectedTile.Icon.transform.DOScale(Vector3.zero,TweenDuration));
+                }
+                await deflateSequence.Play().AsyncWaitForCompletion();
+
+                var inflateSequence = DOTween.Sequence();
+
+                foreach(var connectedTile in connectedTiles)
+                {
+                    connectedTile.Item = ItemDatabase.Items[UnityEngine.Random.Range(0,ItemDatabase.Items.Length)];
+                    inflateSequence.Join(connectedTile.Icon.transform.DOScale(Vector3.one,TweenDuration));
+                }
+                await inflateSequence.Play().AsyncWaitForCompletion();
+                if (CanPopx())
+                {
+                    Popx();
+                }
+                if (CanPopy())
+                {
+                    Popy();
+                }
+            }
+        }
+    }
+    private async void Popy()
+    {
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                var tile = Tiles[x, y];
+                var connectedTiles = tile.GetConnectedTilesy();
+                if (connectedTiles.Skip(1).Count() < 2)
+                {
+                    continue;
+                }
+                var deflateSequence = DOTween.Sequence();
+
+                foreach (var connectedTile in connectedTiles)
+                {
+                    deflateSequence.Join(connectedTile.Icon.transform.DOScale(Vector3.zero, TweenDuration));
+                }
+                await deflateSequence.Play().AsyncWaitForCompletion();
+
+                var inflateSequence = DOTween.Sequence();
+
+                foreach (var connectedTile in connectedTiles)
+                {
+                    connectedTile.Item = ItemDatabase.Items[UnityEngine.Random.Range(0, ItemDatabase.Items.Length)];
+                    inflateSequence.Join(connectedTile.Icon.transform.DOScale(Vector3.one, TweenDuration));
+                }
+                await inflateSequence.Play().AsyncWaitForCompletion();
+                if (CanPopx())
+                {
+                    Popx();
+                }
+                if (CanPopy())
+                {
+                    Popy();
+                }
+            }
+        }
     }
 }
